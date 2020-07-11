d663c98d6b5ed213b34ff7d9caab79888c2348e7
Alireza B <Alireza@simor.org>
2020-07-11 09:48:46 -0600

    encryption

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
+const encrypt = require("mongoose-encryption")
+
 
 const app = express()
 
@@ -15,10 +17,14 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useUnifiedTopology: true})
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email : String,
   password : String,
-};
+});
+
+const secret = "this is our little secret";
+
+userSchema.plugin(encrypt, { secret : secret, excludeFromEncryption: ["password"]  });
 
 const User = new mongoose.model("User", userSchema)
 
