905d423ef88905fce1e088451d21fa470f4e612f
Alireza B <Alireza@simor.org>
2020-07-11 18:26:03 -0600

    add secret button activated and can read all secrets

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const userSchema = new mongoose.Schema ({
   email : String,
   password : String,
   googleID: String,
+  secret : String,
 });
 
 userSchema.plugin.(passportLocalMongoose);
@@ -94,12 +95,40 @@ app.get("/register", function(req,res){
 })
 
 app.get("/secrets",function(req,res){
+  User.find({"secret": {$ne: null}}, function(err,foundUser){
+    if (err){
+      console.log(err);
+    }else{
+      if (foundUser){
+        res.render("secrets", {userWithSecrets: foundUsers})
+      }
+    }
+  })
+})
+
+app.get("/submit", function(req,res){
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
 
   }else{
     res.redirect("/login")
   }
+});
+
+app.post("/submit", function(req,res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function(err,foundUser){
+    if (err){
+      console.log(err);
+    } else {
+      if (foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function()){
+          res.redirect("/secrets")
+        }
+      }
+    }
+  })
 })
 
 app.post("/login", function(req,res){
